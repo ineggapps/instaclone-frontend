7ddd1bd80b43a3f0696e58dc9e9b3dc6516078d1
inegg <inegg.apps@gmail.com>
2019-05-03 15:09:20 +0900

    LogIn function have been implemented!
    LogIn function have been implemented!
    
    1. get email address
    2. send secret token to email address.
    3. authenticate secret token
    4. Log In is processed by saving token in web storage and cache.

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { LOG_IN, CREATE_ACCOUNT, CONFIRM_SECRET, LOCAL_LOG_IN } from "./AuthQueries";
 import { toast } from "react-toastify";
 
 export default () => {
@@ -10,20 +10,14 @@ export default () => {
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
+  const secret = useInput("");
   const email = useInput("inegg.apps@gmail.com");
-  const requestSecret = useMutation(LOG_IN, {
-    update: (_, { data }) => {
-      const { requestSecret } = data;
-      if (!requestSecret) {
-        toast.error("You don't have an account yet, create one");
-        setAction("signUp");
-        setTimeout(() => setAction("signUp", 3000));
-      }
-    },
+  const requestSecretMutation = useMutation(LOG_IN, {
+    update: (_, { data }) => {},
     variables: { email: email.value }
   });
 
-  const createAccount = useMutation(CREATE_ACCOUNT, {
+  const createAccountMutation = useMutation(CREATE_ACCOUNT, {
     variables: {
       email: email.value,
       username: username.value,
@@ -32,11 +26,31 @@ export default () => {
     }
   });
 
-  const onSubmit = e => {
+  const localLogInMutation = useMutation(LOCAL_LOG_IN);
+
+  const confirmSecretMutation = useMutation(CONFIRM_SECRET, {
+    variables: { email: email.value, secret: secret.value }
+  });
+
+  const onSubmit = async e => {
     e.preventDefault();
     if (action === "logIn") {
       if (email.value !== "") {
-        requestSecret();
+        try {
+          const {
+            data: { requestSecret }
+          } = await requestSecretMutation();
+          if (!requestSecret) {
+            toast.error("You don't have an account yet, create one");
+            setAction("signUp");
+            setTimeout(() => setAction("signUp", 3000));
+          } else {
+            toast.success("Check your inbox for your login secret");
+            setAction("confirm");
+          }
+        } catch {
+          toast.error("Can't request secret, try again.");
+        }
       } else {
         toast.error("Email is required");
       }
@@ -47,7 +61,36 @@ export default () => {
         firstName.value !== "" &&
         lastName.value !== ""
       ) {
-        createAccount();
+        try {
+          const {
+            data: { createAccount }
+          } = await createAccountMutation();
+          if (!createAccount) {
+            toast.error("Can't create account, try again");
+          } else {
+            toast.success("Account created! Log In now!");
+            setTimeout(() => setAction("logIn"), 3000);
+          }
+        } catch (e) {
+          toast.error(e.message);
+        }
+      } else {
+        toast.error("All fields are required");
+      }
+    } else if (action === "confirm") {
+      if (secret.value !== "") {
+        try {
+          const {
+            data: { confirmSecret: token }
+          } = await confirmSecretMutation();
+          if (token !== "" && token !== undefined) {
+            localLogInMutation({ variables: { token } });
+          } else {
+            throw Error();
+          }
+        } catch {
+          toast.error("Can't confirm secret, check again!");
+        }
       }
     }
   };
@@ -60,6 +103,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
+      secret={secret}
       onSubmit={onSubmit}
     />
   );
--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -46,15 +46,16 @@ const Form = styled(Box)`
   }
 `;
 
-export default ({ action, username, firstName, lastName, email, setAction, onSubmit }) => (
+export default ({ action, username, firstName, lastName, email, setAction, secret, onSubmit }) => (
   <Wrapper>
     <Form>
-      {action === "logIn" ? (
+      {action === "logIn" && (
         <form onSubmit={onSubmit}>
           <Input placeholder={"Email"} {...email} type="email" />
           <Button text={"Log in"} />
         </form>
-      ) : (
+      )}
+      {action === "signUp" && (
         <form onSubmit={onSubmit}>
           <Input placeholder={"First name"} {...firstName} />
           <Input placeholder={"Last name"} {...lastName} />
@@ -63,17 +64,25 @@ export default ({ action, username, firstName, lastName, email, setAction, onSub
           <Button text={"Sign Up"} />
         </form>
       )}
-    </Form>
-    <StateChanger>
-      {action === "logIn" ? (
-        <>
-          Don't have an account? <Link onClick={() => setAction("signUp")}>Sign up</Link>
-        </>
-      ) : (
-        <>
-          Have an account? <Link onClick={() => setAction("logIn")}>Log in</Link>
-        </>
+      {action === "confirm" && (
+        <form onSubmit={onSubmit}>
+          <Input placeholder="Paste your secret" required {...secret} />
+          <Button text={"Confirm"} />
+        </form>
       )}
-    </StateChanger>
+    </Form>
+    {action !== "confirm" && (
+      <StateChanger>
+        {action === "logIn" ? (
+          <>
+            Don't have an account? <Link onClick={() => setAction("signUp")}>Sign up</Link>
+          </>
+        ) : (
+          <>
+            Have an account? <Link onClick={() => setAction("logIn")}>Log in</Link>
+          </>
+        )}
+      </StateChanger>
+    )}
   </Wrapper>
 );
