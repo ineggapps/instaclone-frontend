2981543be214f01c74b73996b7d760ce2b2ff25b
inegg <inegg.apps@gmail.com>
2019-05-09 22:42:20 +0900

    The Profile component has been fixed!
    The Profile component has been fixed!
    
    1. Added Loader.
    2. Styling subordinate components.
    3. Connected Logout Function using mustation that had been implemented on previous commit history.

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -1,21 +1,70 @@
 import React from "react";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import FatText from "../../Components/FatText";
 import Avatar from "../../Components/Avatar";
 import Loader from "../../Components/Loader";
+import SquarePost from "../../Components/SquarePost";
+import FollowButton from "./../../Components/FollowButton";
+import Button from "../../Components/Button";
 
 const Wrapper = styled.div`
-  min-height: 60vh;
+  min-height: 100vh;
 `;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto 40px;
+  justify-content: space-around;
+`;
 
 const HeaderColumn = styled.div``;
 
-export default ({ loading, data }) => {
+const UsernameRow = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Username = styled.span`
+  font-size: 26px;
+  margin-bottom: 10px;
+  display: block;
+`;
+
+const FullName = styled(FatText)`
+  font-size: 14px;
+`;
+
+const Counts = styled.ul`
+  display: flex;
+  margin: 15px 0px;
+`;
+
+const Count = styled.li`
+  font-size: 15px;
+  &:not(:last-child) {
+    margin-right: 10px;
+  }
+`;
+
+const Bio = styled.p`
+  margin: 10px 0;
+`;
+
+const Posts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
+`;
+
+export default ({ loading, data, logOut }) => {
   if (loading === true) {
     return (
       <Wrapper>
-        <Loader />;
+        <Loader />
       </Wrapper>
     );
   } else if (!loading && data && data.seeUser) {
@@ -29,19 +78,56 @@ export default ({ loading, data }) => {
         isSelf,
         bio,
         followingCount,
-        follwersCount,
+        followersCount,
         postsCount,
         posts
       }
     } = data;
     return (
-      <>
+      <Wrapper>
+        <Helmet>
+          <title>{username} | Profile</title>
+        </Helmet>
         <Header>
           <HeaderColumn>
             <Avatar size="lg" url={avatar} />
           </HeaderColumn>
+          <HeaderColumn>
+            <UsernameRow>
+              <Username>{username}</Username>
+              {isSelf ? (
+                <Button text="Logout" onClick={logOut} />
+              ) : (
+                <FollowButton id={id} isFollowing={isFollowing} />
+              )}
+            </UsernameRow>
+            <Counts>
+              <Count>
+                <FatText text={String(postsCount)} /> posts
+              </Count>
+              <Count>
+                <FatText text={String(followersCount)} /> followers
+              </Count>
+              <Count>
+                <FatText text={String(followingCount)} /> following
+              </Count>
+            </Counts>
+            <FullName text={fullName} />
+            <Bio>{bio}</Bio>
+          </HeaderColumn>
         </Header>
-      </>
+        <Posts>
+          {posts &&
+            posts.map(post => (
+              <SquarePost
+                key={post.id}
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))}
+        </Posts>
+      </Wrapper>
     );
   }
   return null;
